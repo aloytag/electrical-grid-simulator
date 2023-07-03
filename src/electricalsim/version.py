05b7101422a68fdc20e02a2dfd73cf2b56c4a6ef
aloytag <ariel.loyarte@gmail.com>
2023-07-02 22:01:24 -0300

    Change version details

--- src/electricalsim/version.py
+++ src/electricalsim/version.py
@@ -5,6 +5,6 @@ try:
 except PackageNotFoundError:
     VERSION = 'development version'
 
-DATE = '2023-06-30'
-AUTHOR = 'PhD Ariel S. Loyarte'
+DATE = '2023-07-02'
+AUTHOR = 'Dr. Ing. Ariel S. Loyarte'
 CONTACT = 'aloyarte@frsf.utn.edu.ar'
