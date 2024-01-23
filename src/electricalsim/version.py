0e1b0f0b8fa38d87397fd87649353f4cda143d00
aloytag <ariel.loyarte@gmail.com>
2024-01-22 22:17:25 -0300

    Updating numba dependency

--- src/electricalsim/version.py
+++ src/electricalsim/version.py
@@ -5,6 +5,6 @@ try:
 except PackageNotFoundError:
     VERSION = 'development version'
 
-DATE = '2024-01-17'
+DATE = '2024-01-22'
 AUTHOR = 'Dr. Ing. Ariel S. Loyarte'
 CONTACT = 'aloyarte@frsf.utn.edu.ar'
