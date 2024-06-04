86cc177bef945e120ef7a3ab6e986fc99bcfeea9
aloytag <ariel.loyarte@gmail.com>
2024-06-04 16:49:03 -0300

    Change in line length limits

--- src/electricalsim/version.py
+++ src/electricalsim/version.py
@@ -5,6 +5,6 @@ try:
 except PackageNotFoundError:
     VERSION = 'development version'
 
-DATE = '2024-05-20'
+DATE = '2024-06-04'
 AUTHOR = 'Dr. Ing. Ariel S. Loyarte'
 CONTACT = 'aloyarte@frsf.utn.edu.ar'
