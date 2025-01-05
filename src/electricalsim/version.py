4ef2587b264baa8b9f71bf0f58e681eedb20b199
aloytag <ariel.loyarte@gmail.com>
2025-01-05 01:34:00 -0300

    University logo in splash screen

--- src/electricalsim/version.py
+++ src/electricalsim/version.py
@@ -5,6 +5,6 @@ try:
 except PackageNotFoundError:
     VERSION = 'development version'
 
-DATE = '2024-01-04'
+DATE = '2024-01-05'
 AUTHOR = 'Dr. Ing. Ariel S. Loyarte'
 CONTACT = 'aloyarte@frsf.utn.edu.ar'
